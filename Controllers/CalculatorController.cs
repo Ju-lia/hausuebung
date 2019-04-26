4163ad7960df093389154d69ac204af42ad99ba2
jneumann <Julia.Neumann@eventim.de>
2019-04-26 12:01:18 +0200

    Start ignoring

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -13,15 +13,15 @@ namespace juliasMicroservice.Controllers
         public ActionResult<string> Post([FromBody] string value)
         {
             Calculator calculator = new Calculator();
-                calculator.Add(value);
-            return "" + calculator.Add(value);
             try
             {
+                calculator.Add(value);
             }
             catch (Exception e)
             {
                 return "Es ist ein unerwarteter Fehler aufgetreten, ihr PC wird nun heruntergefahren.";
             }
+            return "" + calculator.Add(value);
 
 
         }
